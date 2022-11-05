9dfea25a924ed7a66da1ab2bdb0f2ee73e34c0bc
livisson teixeira <livisson.teixeira@fatec.sp.gov.br>
2022-11-04 21:28:41 -0300

    commit inicial

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+//hooks do react
+
 
 export default function App() {
+  const [contador, setContador] = useState(0)
+
+  const incrementar = () => {
+    setContador(contador + 1) 
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{contador}</Text>
+      <Button
+        title='incrementar'
+        onPress={incrementar}
+      />
       <StatusBar style="auto" />
     </View>
   );
